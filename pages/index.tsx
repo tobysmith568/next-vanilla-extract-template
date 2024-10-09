840bc80a258deafb751f1d1e89f8bfd7a3da5a63
Toby Smith <tobysmith568@hotmail.co.uk>
2024-10-09 23:46:06 +0100

    Switch to pages dir

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Welcome } from "../components/Welcome/Welcome";
+import { ColorSchemeToggle } from "../components/ColorSchemeToggle/ColorSchemeToggle";
+
+const Page = () => (
+	<>
+		<Welcome />
+		<ColorSchemeToggle />
+	</>
+)
+
+export default Page;
